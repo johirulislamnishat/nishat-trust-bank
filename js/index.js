6dd93f842b61be20736128b7999bff351a76d405
Johirul Islam Nishat <jahirulislamn@gmail.com>
2021-09-09 20:18:42 +0600

    sidebar and style update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+// SideBar
+let sideBar = document.getElementById("mobile-nav");
+let openSidebar = document.getElementById("openSideBar");
+let closeSidebar = document.getElementById("closeSideBar");
+sideBar.style.transform = "translateX(-260px)";
+
+function sidebarHandler(flag) {
+    if (flag) {
+        sideBar.style.transform = "translateX(0px)";
+        openSidebar.classList.add("hidden");
+        closeSidebar.classList.remove("hidden");
+    } else {
+        sideBar.style.transform = "translateX(-260px)";
+        closeSidebar.classList.add("hidden");
+        openSidebar.classList.remove("hidden");
+    }
+}
+
+
 // common getInput function
 function getInputValue(inputID) {
     const inputField = document.getElementById(inputID);
